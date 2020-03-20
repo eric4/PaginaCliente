6c1d6279442dd838203611b336bdfe7330c549f5
Eric <eric_cp4@hotmail.com>
2020-03-20 12:03:53 +0100

    ASUCAR

--- Git_Project/src/com/sistema/init/Start.java
+++ Git_Project/src/com/sistema/init/Start.java
@@ -4,10 +4,10 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("YEPA AQUIIII!");
-		System.out.println("YEPA AQUIIII!");
-		System.out.println("YEPA AQUIIII!");
-		System.out.println("YEPA AQUIIII!");
-		System.out.println("YEPA AQUIIII!");
+		System.out.println("ASUCAR!");
+		System.out.println("ASUCAR!");
+		System.out.println("ASUCAR!");
+		System.out.println("ASUCAR!");
 	}
 
 }
